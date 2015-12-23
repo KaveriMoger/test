bc4fd2106d286ecddcccb908e2b2df5214db11e6
kaveri <kaveribkl19@gmail.com>
2015-12-23 18:14:26 +0530

    save

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,49 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  belongs_to :role
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
-  def has_role?(role_name)
-    roles.pluck(:name).member?(role_name.to_s)
+  #attr_accessible :email, :password, :password_confirmation
+  require 'bcrypt'
+  silence_warnings do BCrypt::Engine::DEFAULT_COST = BCrypt::Engine::MIN_COST
   end
-         
+  has_secure_password validations: false
+  attr_accessor :password
+  before_save :encrypt_password
+  before_create { generate_token(:auth_token) }
+  validates_confirmation_of :password
+  validates_presence_of :password, :on => :create
+  validates_presence_of :email
+  validates_uniqueness_of :email
+  #validates :password_confirmation, presence: true, if: :password_digest_changed?
+
+  def self.authenticate(email, password)
+    user = find_by_email(email)
+    if user && user.password_hash == BCrypt::Engine.hash_secret(password, user.password_salt)
+      user
+    else
+      nil
+    end
+  end
+
+  def encrypt_password
+    if password.present?
+      self.password_salt = BCrypt::Engine.generate_salt
+      self.password_hash = BCrypt::Engine.hash_secret(password, password_salt)
+    end
+  end
+
+
+
+
+def generate_token(column)
+  begin
+    self[column] = SecureRandom.urlsafe_base64
+  end while User.exists?(column => self[column])
+end
+
+
+def send_password_reset
+  generate_token(:password_reset_token)
+  self.password_reset_sent_at = Time.zone.now
+  save!
+  UserMailer.password_reset(self).deliver
+end
+
 end
