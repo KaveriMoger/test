34665479a86eaad02463c0ab1c531d228d214194
kaveri <kaveribkl19@gmail.com>
2016-01-20 18:54:24 +0530

    save

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,10 @@
 class UsersController < ApplicationController
+  before_action :current_user, :except => [:new, :create]
+  before_action :admin_only, :only => [:destroy]
+  def index
+    @user=User.all
+    @user = User.search(params[:search])
+  end
 
   def new
     @user = User.new
@@ -12,10 +18,32 @@ class UsersController < ApplicationController
       render "new"
     end
   end
+  def show
+    @user=User.find(params[:id])
+  end
+
+  def edit
+    @user=User.find(params[:id])
+  end
+
+  def update
+    @user=User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @user=User.find(params[:id])
+    @user.destroy
+
+    redirect_to users_path
+  end
 
   private
   def user_params
-    params.require(:user).permit(:email, :password, :password_confirmation)
+    params.require(:user).permit(:email, :password, :password_confirmation, :role_id)
   end
-
 end
