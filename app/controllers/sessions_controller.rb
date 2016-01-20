63fdf90efb6e035c842b6ac9d5da6a97246a08f5
kaveri <kaveribkl19@gmail.com>
2016-01-20 18:53:03 +0530

    save

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,36 @@
+class SessionsController < ApplicationController
+
+  def new
+
+  end
+
+  def create
+    user = User.find_by_email(params[:email])
+    if user && user.authenticate(params[:password])
+
+      if params[:remember_me]
+
+        cookies.permanent[:auth_token] = user.auth_token
+        #admin_only
+      else
+        cookies[:auth_token] = user.auth_token
+      end
+
+      redirect_to documents_path
+    else
+      flash.now.alert = "Invalid email or password"
+      render "new"
+    end
+  end
+
+  def destroy
+    cookies.delete(:auth_token)
+    redirect_to root_url, :notice => "Logged out!"
+  end
+
+end
+
+
+
+
+
