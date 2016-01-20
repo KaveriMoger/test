d0f52b38f3697b1e2045df14ddead575d2c01599
kaveri <kaveribkl19@gmail.com>
2016-01-20 18:58:20 +0530

    save

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,5 +1,14 @@
 class Team < ActiveRecord::Base
-	validates :team_name, :team_head, :summary, :presence => true
-	has_many :documents
-	has_and_belongs_to_many :clients
+  validates :team_name, :team_head, :summary, :presence => true
+  has_many :documents
+  has_and_belongs_to_many :clients
+
+
+
+
+  def self.search(search)
+    # where(:title, query) -> This would return an exact match of the query
+    where("team_name like ?", "%#{search}%")
+
+  end
 end
