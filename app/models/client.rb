df637d98a5c4409e5fbcd9ccd8f9c0edbd71f3e6
kaveri <kaveribkl19@gmail.com>
2016-01-20 18:56:04 +0530

    save

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,5 +1,11 @@
 class Client < ActiveRecord::Base
-validates :c_name, :c_email, :c_website, :c_country, presence:true
-has_and_belongs_to_many :documents
-has_and_belongs_to_many :teams
+  validates :c_name, :c_email, :c_website, :c_country, presence:true
+  has_and_belongs_to_many :documents
+  has_and_belongs_to_many :teams
+
+  def self.search(search)
+    # where(:title, query) -> This would return an exact match of the query
+    where("c_name like ?", "%#{search}%")
+
+  end
 end
