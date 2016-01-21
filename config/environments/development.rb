cd3892c1d59296e34cf17ced7fd11990b91e4cc0
kaveri <kaveribkl19@gmail.com>
2016-01-21 10:44:42 +0530

    save

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -32,7 +32,7 @@ Project1::Application.configure do
 
 
 
-config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+config.action_mailer.default_url_options = { :host => "localhost:3000" }
 
 
 
